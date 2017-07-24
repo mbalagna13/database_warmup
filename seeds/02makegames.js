230dc521524f419ece9e1e36afb2185d19279521
Michael Balagna <lunchmeat13@gmail.com>
2017-07-24 10:23:15 -0600

    fixed seed stuff

--- seeds/02makegames.js
+++ seeds/02makegames.js
@@ -4,12 +4,12 @@ exports.seed = function(knex, Promise) {
   return knex.raw('TRUNCATE users RESTART IDENTITY CASCADE;')
     .then(function () {
       return knex('games').insert([
-        {id:1, name: 'SSBM', type:'2d platform fighting', user_id:"1"},
-        {id:2, name: 'MVC3', type:'2d fighting', user_id:"1"},
-        {id:2, name: 'MVC3', type:'2d fighting', user_id:"2"},
-        {id:2, name: 'MVC3', type:'2d fighting', user_id:"3"},
-        {id:3, name: 'COD4', type:'FPS', user_id:"2"},
-        {id:2, name: 'COD4', type:'FPS', user_id:"1"}
+        { name: 'SSBM', type:'2d platform fighting', user_id:"1"},
+        { name: 'MVC3', type:'2d fighting', user_id:"1"},
+        { name: 'MVC3', type:'2d fighting', user_id:"2"},
+        { name: 'MVC3', type:'2d fighting', user_id:"3"},
+        { name: 'COD4', type:'FPS', user_id:"2"},
+        { name: 'COD4', type:'FPS', user_id:"1"}
 
       ])
       // Inserts seed entries
