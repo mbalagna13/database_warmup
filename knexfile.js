18e49596202317f6010173b087792edddc7ec5ac
Michael Balagna <lunchmeat13@gmail.com>
2017-07-26 11:18:11 -0600

    fixed thigns

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,14 @@
-// Update with your config settings.
+const dotenv = require('dotenv')
 
 module.exports = {
 
   development: {
     client: 'pg',
     connection: 'postgres://localhost/mikegaming'
+  },
+  production:{
+    client:'pg',
+    connection: process.env.DATABASE_URL
   }
 
 };
